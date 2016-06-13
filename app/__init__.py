2276159e98dfac7f5b4c381c0cfbe387a10f8d19
Chris Shepard <csshepard@gmail.com>
2016-06-13 15:51:25 -0400

    added redis listening to musicblocks

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config, Config
-from celery import Celery
+from config import config
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
-celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,12 +17,10 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
-    celery.conf.update(app.config)
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
 
     return app
