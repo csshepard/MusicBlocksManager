ce57b1856a0845d2f158bf86e8d6ed4befaca8b0
Chris Shepard <csshepard@gmail.com>
2016-06-20 22:35:07 -0400

    added static style and js files

---
+++ app/static/script.js
@@ -0,0 +1,22 @@
+function setPlayerState(data){
+    if(data.active && $('#ps_active').is(':visible')){
+        $('#ps_active').hide();
+        $('#btn-volume').prop('disabled', false);
+        $('#input-volume').slider('setValue', parseFloat(data.volume))
+    }
+    else if (!data.active && $('#ps_active').is(':hidden')){
+        $('#ps_active').show();
+        $('#btn-volume').prop('disabled', true);
+    }
+    $('#ps_volume').html(data.volume+'%');
+    $('#ps_playing').html(data.playing);
+}
+$('#input-volume').slider({
+    formatter: function(value) {
+        return value + '%';
+    }
+});
+function statusBarUpdate(url){
+    $.getJSON(url, function(data) {setPlayerState(data);});
+    setInterval(function(){$.getJSON(url, function(data) {setPlayerState(data);})},1000)
+}
